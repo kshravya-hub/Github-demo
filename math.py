74c144e03f7b12ace4b6b6ebe5c355559a597dd7
kshravya-hub <80024285+kshravya-hub@users.noreply.github.com>
2021-03-04 11:46:35 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,17 @@
+# Add implementation
+def add(x,y):
+	return x+y
+
+# Subtract implementation
+def subtract(x,y):
+    if x>y:
+	return ERROR
+	return x-y
+
+# Multiply implementation
+def multiply(x,y):
+	pass
+
+# Divide implementation
+def divide(x,y):
+    pass
